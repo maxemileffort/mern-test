2ac5723876786bfe6448ce48a925685c799e17ec
Maxemillian Wood <Max W>
2018-08-29 23:41:13 -0500

    trying hardcoding the port

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const config = {
   mongoURL: process.env.MONGO_URL || 'mongodb://admin:admin123@ds135952.mlab.com:35952/max-test',
-  port: process.env.PORT || 8000,
+  port: 8000,
 };
 
 export default config;
